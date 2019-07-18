8f7f9ccc3b457ac2fe5f15b4b7db1e7c34ed26a7
Sophie Gu <sophieqgu@gmail.com>
2019-07-18 05:12:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,4 +2,10 @@ class Application
   def call(env)
     current_time = Time.now 
     
-    if current_time.hour
+    if current_time.hour < 12 
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end 
+  end 
+end 
